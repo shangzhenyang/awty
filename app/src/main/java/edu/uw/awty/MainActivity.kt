b74bbdaacfffea6a740c0152acc6f129b5a27485
Shangzhen Yang <shangzheny01@gmail.com>
2022-02-21 17:09:45 -0800

    Complete part2

--- app/src/main/java/edu/uw/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/awty/MainActivity.kt
@@ -1,11 +1,13 @@
 package edu.uw.awty
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     var serviceStarted = false
@@ -14,54 +16,83 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (checkSelfPermission(android.Manifest.permission.SEND_SMS) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.SEND_SMS),
+                0
+            )
+        }
+
         val inputMsg = findViewById<EditText>(R.id.input_msg)
         val inputPhoneNum = findViewById<EditText>(R.id.input_phone_num)
         val inputInterval = findViewById<EditText>(R.id.input_interval)
         val btnStart = findViewById<Button>(R.id.btn_start)
 
         btnStart.setOnClickListener {
-            if (serviceStarted) {
-                stopService(Intent(applicationContext, MsgService::class.java))
-                serviceStarted = false
-                btnStart.text = getString(R.string.start)
-            } else {
-                try {
-                    when {
-                        inputMsg.text.isEmpty() -> {
-                            Toast.makeText(
-                                this,
-                                getString(R.string.message_empty),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        inputPhoneNum.text.toString().length != 10 -> {
-                            Toast.makeText(
-                                this,
-                                getString(R.string.invalid_phone_number),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        inputInterval.text.isEmpty() ||
-                                inputInterval.text.toString().toInt() <= 0 -> {
-                            Toast.makeText(
-                                this,
-                                getString(R.string.interval_must_greater_zero),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        else -> {
-                            val intent = Intent(applicationContext, MsgService::class.java).apply {
-                                putExtra("msg", inputMsg.text.toString())
-                                putExtra("phone_num", inputPhoneNum.text.toString())
-                                putExtra("interval", inputInterval.text.toString().toInt())
+            when {
+                checkSelfPermission(android.Manifest.permission.SEND_SMS) !=
+                        PackageManager.PERMISSION_GRANTED -> {
+                    AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.permission_denied))
+                        .setMessage(getString(R.string.cannot_work_without_sms_permission))
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.ok, null)
+                        .show()
+                }
+                serviceStarted -> {
+                    stopService(Intent(applicationContext, MsgService::class.java))
+                    serviceStarted = false
+                    btnStart.text = getString(R.string.start)
+                }
+                else -> {
+                    try {
+                        when {
+                            inputMsg.text.isEmpty() -> {
+                                AlertDialog.Builder(this)
+                                    .setTitle(getString(R.string.error))
+                                    .setMessage(getString(R.string.message_empty))
+                                    .setIcon(android.R.drawable.ic_dialog_alert)
+                                    .setPositiveButton(android.R.string.ok, null)
+                                    .show()
                             }
-                            startForegroundService(intent)
-                            serviceStarted = true
-                            btnStart.text = getString(R.string.stop)
+                            inputPhoneNum.text.toString().length != 10 -> {
+                                AlertDialog.Builder(this)
+                                    .setTitle(getString(R.string.error))
+                                    .setMessage(getString(R.string.invalid_phone_number))
+                                    .setIcon(android.R.drawable.ic_dialog_alert)
+                                    .setPositiveButton(android.R.string.ok, null)
+                                    .show()
+                            }
+                            inputInterval.text.isEmpty() ||
+                                    inputInterval.text.toString().toInt() <= 0 -> {
+                                AlertDialog.Builder(this)
+                                    .setTitle(getString(R.string.error))
+                                    .setMessage(getString(R.string.interval_must_greater_zero))
+                                    .setIcon(android.R.drawable.ic_dialog_alert)
+                                    .setPositiveButton(android.R.string.ok, null)
+                                    .show()
+                            }
+                            else -> {
+                                val intent = Intent(applicationContext, MsgService::class.java).apply {
+                                    putExtra("msg", inputMsg.text.toString())
+                                    putExtra("phone_num", inputPhoneNum.text.toString())
+                                    putExtra("interval", inputInterval.text.toString().toInt())
+                                }
+                                startForegroundService(intent)
+                                serviceStarted = true
+                                btnStart.text = getString(R.string.stop)
+                            }
                         }
+                    } catch (err: Exception) {
+                        AlertDialog.Builder(this)
+                            .setTitle(getString(R.string.error))
+                            .setMessage(err.toString())
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setPositiveButton(android.R.string.ok, null)
+                            .show()
                     }
-                } catch (err: Exception) {
-                    Toast.makeText(this, err.toString(), Toast.LENGTH_SHORT).show()
                 }
             }
 
--- app/src/main/java/edu/uw/awty/MsgService.kt
+++ app/src/main/java/edu/uw/awty/MsgService.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.telephony.SmsManager
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationCompat.PRIORITY_MIN
@@ -56,11 +57,9 @@ class MsgService : Service() {
         msg = intent?.getStringExtra("msg").toString()
         phoneNum = intent?.getStringExtra("phone_num").toString()
         interval = intent?.getIntExtra("interval", 0)!!
-        val toastText = "(" + phoneNum.substring(0, 3) + ") " + phoneNum.substring(3, 6) + "-" +
-                phoneNum.substring(6) + ": " + msg
         timer = Timer()
         timer.scheduleAtFixedRate(
-            DisplayToastTimerTask(this, handler, toastText),
+            SendSmsTimerTask(context, handler, phoneNum, msg),
             0,
             (interval * 60000).toLong()
         )
@@ -73,14 +72,17 @@ class MsgService : Service() {
         Toast.makeText(this, "Service Ended", Toast.LENGTH_SHORT).show()
     }
 
-    private class DisplayToastTimerTask(
+    private class SendSmsTimerTask(
         val context: Context,
         val handler: Handler,
-        val text: String
+        val phoneNum: String,
+        val msg: String
     ) : TimerTask() {
         override fun run() {
             handler.post(Runnable {
-                Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+                val smsManager = SmsManager.getDefault()
+                smsManager.sendTextMessage(phoneNum, null, msg, null, null)
+                Toast.makeText(context, "SMS Sent", Toast.LENGTH_SHORT).show()
             })
         }
     }
